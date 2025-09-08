3cee8f6657e4e56b37f879e40324037cc0f6c1ed
Ale Benavides Loo <albenaloo27@gmail.com>
2025-09-08 18:01:27 -0400

    refactored to extraxt helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,30 +7,64 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        doFizzBuzz();
+    }
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+    private static void doFizzBuzz() {
+        int x = 1;
+        boolean divBy3 = false;
+        boolean divBy5 = false;
+        boolean thing = x < 100;
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+        while (thing) { // Using a while function instead of a for loop
 
+            if (divBy3 && divBy5) {
                 System.out.println("Fizz Buzz");
+            }
 
-            } else if (divisibleBy3) {
-
+            else if (divBy3) {
                 System.out.println("Fizz");
+            }
 
-            } else if (divisibleBy5) {
+            else if (divBy5) {
 
                 System.out.println("Buzz");
 
             } else {
 
-                System.out.println(i);
+                System.out.println(x);
 
             }
+            x += 1;
+            thing = x < 100;
+            divBy5 = x % 5 == 0;
+            divBy3 = x % 3 == 0;
+        }
+
+//        for (int i = 1; i < 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) { // && instead of and
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) { //else if instead of if
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+//        }
         }
     }
-}
