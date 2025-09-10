72425b9ceb5b9523576a38af6f43fee9f090eafd
Ale Benavides Loo <albenaloo27@gmail.com>
2025-09-09 22:22:11 -0400

    fixed a variable

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -7,7 +7,7 @@ public class Reduce {
     }
 
     public static int reduce(int n) {
-        int count = 1;
+        int count = 0;
         while (n != 0) {
             if (n % 2 == 0) {
                 n = n / 2;
