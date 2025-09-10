6e7f359a6425ee456c083913a16f1a9f644edbc8
Ale Benavides Loo <albenaloo27@gmail.com>
2025-09-09 22:18:36 -0400

    Reduce method

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,24 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        reduce(100);
+
+    }
+
+    public static void reduce(int n) {
+        int count = 1;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                count += 1;
+            }
+
+            else {
+                n = n -1;
+                count += 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
